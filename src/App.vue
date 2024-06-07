a42b437858a9674ffdd7ea953d622e29e2a44464
Gaetano Frascolla <gaetano.frascolla@gmail.com>
2024-06-07 10:05:59 +0200

    feat: button event searchMovies

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
-<!-- <script></script> -->
+<script>
+export default {
+  name: 'BoolFlix',
+  methods: {
+    searchMovies() {
+      console.log('ho cliccato sul bottone');
+    },
+  },
+};
+</script>
 <template>
-  <h1>Vue Vite Template</h1>
-  <p>Template di partenza per progetti Vite Vue</p>
+  <h1>BoolFlix</h1>
+
+  <div>
+    <label for="search-movie">Ricerca un film</label>
+    <input type="text" id="search-movie" />
+    <button @click="searchMovies">Cerca</button>
+  </div>
 </template>
 <!-- <style></style> -->
